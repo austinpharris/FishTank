c9c738391d72cc97634dd13aa56116106ff81e49
Austin Harris <austinpharris@outlook.com>
2022-10-26 21:13:52 -0700

    UTIL: getPostData

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/productModel');
+const { getPostData } = require('../utils');
 
 //@desc     Get all products
 //@route    GET /api/products
@@ -36,29 +37,20 @@ async function getProduct(req, res, id) {
 //@route    POST /api/products
 async function createProduct(req, res) {
     try{
-        const test_product = {
-            title: 'test Product',
-            description: 'This is a test product description',
-            price: 100
-        }
-
-        let body = '';
-        req.on('data', (chunk) => {
-            body += chunk.toString();
-        })
-        req.on('end', async () => {
-            const { id, title, description, price } = JSON.parse(body);
-            const product = {
-                id,
-                title,
-                description,
-                price
-            }            
-            const newProduct = await Product.create(product);
-            res.writeHead(201, {'Content-Type': 'application/json'});
-            res.write(JSON.stringify( newProduct));
-            res.end()
-        })
+        const body = await getPostData(req);
+        
+        const { id, title, description, price } = JSON.parse(body);
+        const product = {
+            id,
+            title,
+            description,
+            price
+        }            
+        const newProduct = await Product.create(product);
+        res.writeHead(201, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify( newProduct));
+        res.end()
+        
     } catch(error){
         console.log(error);
     }
